03fdabcaca1038ccc7fb59527869a69ff7cc46cd
Nicolas Premont <npremont@shi-r2-p1.s19.be>
2023-10-18 12:19:20 +0200

    secure calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: npremont <npremont@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 10:57:13 by npremont          #+#    #+#             */
-/*   Updated: 2023/10/17 17:12:28 by npremont         ###   ########.fr       */
+/*   Updated: 2023/10/18 12:18:27 by npremont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_calloc(size_t count, size_t size)
 	size_t	total;
 
 	total = count * size;
+	if (size && total / size != count)
+		return (NULL);
 	ptr = (void *)malloc(total);
 	if (ptr != NULL)
 		ft_memset(ptr, 0, total);
