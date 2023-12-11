3a9acc479bb56fa7dd8540160e382b8969732a73
npremz <78978503+npremz@users.noreply.github.com>
2023-12-11 11:57:46 +0100

    Remove duplicate ft_strlen

--- chars.c
+++ chars.c
@@ -12,16 +12,6 @@
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *str)
-{
-	size_t	i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
 int	ft_putchar(char c)
 {
 	if (write(1, &c, 1) == -1)
